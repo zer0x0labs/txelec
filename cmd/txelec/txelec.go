60fb9bbeb60e6304c45598002d85b7f6fd348efa
karl_lockhart <karl.lockhart@gmail.com>
2019-09-08 06:57:07 -0500

    Cleaning up

--- cmd/txelec/txelec.go
+++ cmd/txelec/txelec.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"github.com/sirupsen/logrus"
-	"github.com/spf13/viper"
 	"github.com/zer0x0labs/txelec"
 )
 
@@ -12,27 +11,6 @@ func main() {
 		logrus.Fatal(err)
 	}
 
-	url, err := txelec.GetLatestDLURL(viper.GetString("sources.lmp_index"))
-	if err != nil {
-		logrus.Fatal(err)
-	}
-
-	prices, err := txelec.GetDataFromZippedCSV(url.URL)
-	if err != nil {
-		logrus.Fatal(err)
-	}
-
-	url, err = txelec.GetLatestDLURL(viper.GetString("sources.adders_index"))
-	if err != nil {
-		logrus.Fatal(err)
-	}
-
-	adders, err := txelec.GetDataFromZippedCSV(url.URL)
-	if err != nil {
-		logrus.Fatal(err)
-	}
-
-	pa := txelec.PricesWithAdders(prices, adders)
-	logrus.Info(pa)
-
+	prices := txelec.LatestPrices()
+	logrus.Info(prices)
 }
---
+++ txelec.go
@@ -0,0 +1,31 @@
+package txelec
+
+import (
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+)
+
+// LatestPrices returns the latest prices with adders
+func LatestPrices() []Price {
+	url, err := GetLatestDLURL(viper.GetString("sources.lmp_index"))
+	if err != nil {
+		logrus.Fatal(err)
+	}
+
+	prices, err := GetDataFromZippedCSV(url.URL)
+	if err != nil {
+		logrus.Fatal(err)
+	}
+
+	url, err = GetLatestDLURL(viper.GetString("sources.adders_index"))
+	if err != nil {
+		logrus.Fatal(err)
+	}
+
+	adders, err := GetDataFromZippedCSV(url.URL)
+	if err != nil {
+		logrus.Fatal(err)
+	}
+
+	return PricesWithAdders(prices, adders)
+}
